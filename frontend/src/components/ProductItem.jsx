ea022516f3e88f7310f1bc4b2ec7f9dc07471d00
JoysonStanly <joysonstanley3@gmail.com>
2026-01-30 23:03:47 +0530

    added latest collection

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { Link } from 'react-router-dom'
+
+const ProductItem = ({ id, name, price, image }) => {
+
+  const { currency } = useContext(ShopContext)
+
+  return (
+    <Link to={`/product/${id}`} className='cursor-pointer text-gray-700'>
+
+      <div className='overflow-hidden'>
+        <img
+          src={image}
+          alt={name}
+          className='hover:scale-110 transition ease-in-out duration-300'
+        />
+      </div>
+
+      <p className='pt-3 pb-1 text-sm'>{name}</p>
+      <p className='text-sm font-medium'>{currency}{price}</p>
+
+    </Link>
+  )
+}
+
+export default ProductItem
